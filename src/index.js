0e6eb98cb17cd40d944aaec6aba80270eeb5faa9
Aung <aungksoe.mobile@gmail.com>
2025-09-14 14:06:50 +1000

    added a sample js code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // This file serves as the entry point for the application. It may contain the main logic or functionality of the project.
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello, world!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
